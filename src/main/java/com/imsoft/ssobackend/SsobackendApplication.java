a796e2ac2a6608b54196c32793bfbccae0347775
Roy <roy.zhang@imsoft.com.tw>
2025-10-05 19:16:44 +0800

    chore: initial project setup

---
+++ src/main/java/com/imsoft/ssobackend/SsobackendApplication.java
@@ -0,0 +1,12 @@
+package com.imsoft.ssobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsobackendApplication {
+	public static void main(String[] args) {
+        SpringApplication.run(SsobackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imsoft/ssobackend/SsobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imsoft.ssobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
